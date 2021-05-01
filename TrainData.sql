b6c3af49f9de605454694f47f87058767bc2e974
Jeffrey S <70288418+jeff00363@users.noreply.github.com>
2021-04-30 21:45:51 -0400

    Update TrainData.sql
    error fixing halfway

--- TrainData.sql
+++ TrainData.sql
@@ -1,50 +1,50 @@
-CREATE DATABASE trainData;
-    USE trainData;
-
-        CREATE TABLE passenger (
-        passenger_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT ,
+CREATE DATABASE [IF NOT EXISTS] railway_data;
+    
+USE railway_data;
+    CREATE TABLE passenger (
+        passenger_id INT NOT NULL AUTO_INCREMENT,
         fullname varchar(25) NOT NULL,
         username VARCHAR(50) NOT NULL UNIQUE,
         passwords VARCHAR(255) NOT NULL,
         phoneNumber INT NOT NULL,
         email varchar(50) NOT NULL,
         gender varchar(10) NOT NULL,
-        PRIMARY KEY (userid,username)
-        );
+    PRIMARY KEY (passenger_id,username)
+    );
 
-        CREATE TABLE train (
-        train_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    CREATE TABLE train (
+        train_id INT NOT NULL AUTO_INCREMENT,
         depature_station varchar(25) NOT NULL,
         destination_station VARCHAR(25) NOT NULL UNIQUE,
-        depature_time INT,
-        est_arrival_time INT,
-        depature_date INT,
-        arrival_date INT,
-        number_stops INT,
-        train_capacity INT,
-        depature_track INT,
-        PRIMARY KEY (train_id,depature_station)
-        );
+        depature_time INT NOT NULL,
+        est_arrival_time INT NOT NULL,
+        depature_date INT NOT NULL,
+        arrival_date INT NOT NULL,
+        number_stops INT NOT NULL,
+        train_capacity INT NOT NULL,
+        depature_track INT NOT NULL,
+    PRIMARY KEY (train_id,depature_station)
+    );
 
-        CREATE TABLE ticket (
-        ticket_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        passenger_id INT,
-        train_id INT,
+    CREATE TABLE ticket (
+        ticket_id INT NOT NULL AUTO_INCREMENT,
+        passenger_id INT NOT NULL,
+        train_id INT NOT NULL,
         ticket_class VARCHAR(25) NOT NULL UNIQUE,
-        price INT,
-        trip_date INT,
-        tracker_number INT,
+        price INT NOT NULL,
+        trip_date INT NOT NULL,
+        tracker_number INT NOT NULL,
         PRIMARY KEY (ticket_id)
         FOREIGN KEY (train_id) REFERENCES train,
         FOREIGN KEY (passenger_id) REFERENCES passenger,
         );
 
     CREATE TABLE station (
-        ticket_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        train_id INT,
+        ticket_id INT NOT NULL AUTO_INCREMENT,
+        train_id INT NOT NULL,
         city VARCHAR(25) NOT NULL UNIQUE,
         state_us VARCHAR(25) NOT NULL UNIQUE,
-        num_tracks INT,
+        num_tracks INT NOT NULL,
         PRIMARY KEY (station_id)
         FOREIGN KEY (train_id) REFERENCES train,
         );
